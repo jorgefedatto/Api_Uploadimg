b4a3c0ef9917679835d1368706e992d044881b7e
jorgefedatto <jorgefedatto@gmail.com>
2023-08-17 16:12:56 -0300

    Fix: loginController

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -48,7 +48,7 @@ exports.create = async (req, res) => {
     }
 };
 
-exports.private = checkToken, async (req, res) => {
+exports.private = async (req, res) => {
     const id = req.params.id;
 
     const user = await User.findById(id, '-password');
@@ -59,24 +59,3 @@ exports.private = checkToken, async (req, res) => {
 
     res.status(200).json({ user });
 }
-
-function checkToken(req, res, next){
-
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(" ")[1]
-
-    if(!token){
-        return res.status(401).json({ msg:'Acesso Negado'});
-    }
-
-    try {
-      const secret = process.env.SECRET;
-      
-      jwt.verify(token, secret);
-
-      next();
-    } catch (error) {
-        res.status(400).json({ msg: "Token Inválido"})
-    }
-}
-
--- src/routes/login.js
+++ src/routes/login.js
@@ -7,6 +7,26 @@ const { Module } = require("module");
 
 router.post("/", LoginController.create);
 
-router.post("/user/:id", LoginController.private);
+router.post("/user/:id", checkToken, LoginController.private);
+
+function checkToken(req, res, next){
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({ msg:'Acesso Negado'});
+    }
+
+    try {
+      const secret = process.env.SECRET;
+      
+      jwt.verify(token, secret);
+
+      next();
+    } catch (error) {
+        res.status(400).json({ msg: "Token Inválido"})
+    }
+}
 
 module.exports = router;
